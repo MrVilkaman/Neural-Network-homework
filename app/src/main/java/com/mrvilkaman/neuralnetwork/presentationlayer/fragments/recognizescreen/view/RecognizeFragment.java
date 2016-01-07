76f35706885518fa90e1fbfab31b02838a600a79
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-01-07 17:09:02 +0300

    Added text grid; added buttons panel

--- app/src/main/java/com/mrvilkaman/neuralnetwork/presentationlayer/fragments/recognizescreen/view/RecognizeFragment.java
+++ app/src/main/java/com/mrvilkaman/neuralnetwork/presentationlayer/fragments/recognizescreen/view/RecognizeFragment.java
@@ -4,6 +4,7 @@ package com.mrvilkaman.neuralnetwork.presentationlayer.fragments.recognizescreen
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.mrvilkaman.neuralnetwork.R;
 import com.mrvilkaman.neuralnetwork.presentationlayer.fragments.core.view.BaseFragment;
@@ -11,8 +12,16 @@ import com.mrvilkaman.neuralnetwork.presentationlayer.fragments.recognizescreen.
 import com.mrvilkaman.neuralnetwork.presentationlayer.fragments.recognizescreen.presenter.RecognizePresenterImpl;
 import com.mrvilkaman.neuralnetwork.presentationlayer.fragments.recognizescreen.view.RecognizeView;
 
+import butterknife.Bind;
+
 public class RecognizeFragment extends BaseFragment<RecognizePresenter> implements RecognizeView {
 
+	@Bind(R.id. recognise_true)
+	Button recogniseTrue;
+	@Bind(R.id. recognise)
+	Button recognise;
+	@Bind(R.id. recognise_false)
+	Button recogniseFalse;
 
 	public static RecognizeFragment open() {
 		return new RecognizeFragment();
---
+++ app/src/main/java/com/mrvilkaman/neuralnetwork/presentationlayer/views/TextGridView.java
@@ -0,0 +1,131 @@
+package com.mrvilkaman.neuralnetwork.presentationlayer.views;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.Random;
+
+public class TextGridView extends View {
+
+	private int numColumns, numRows;
+	private int cellWidth, cellHeight;
+	private Paint blackPaint = new Paint();
+	private int[][] cellChecked;
+
+	private boolean black;
+
+	public TextGridView(Context context) {
+		super(context);
+		init();
+	}
+
+	public TextGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public TextGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
+
+	private void init() {
+		blackPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+		setNumColumns(10);
+		setNumRows(10);
+	}
+
+	public void setNumColumns(int numColumns) {
+		this.numColumns = numColumns;
+		calculateDimensions();
+	}
+
+	public int getNumColumns() {
+		return numColumns;
+	}
+
+	public void setNumRows(int numRows) {
+		this.numRows = numRows;
+		calculateDimensions();
+	}
+
+	public int getNumRows() {
+		return numRows;
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		calculateDimensions();
+	}
+
+	private void calculateDimensions() {
+		if (numColumns == 0 || numRows == 0)
+			return;
+
+		cellWidth = getWidth() / numColumns;
+		cellHeight = getHeight() / numRows;
+
+		cellChecked = new int[numColumns][numRows];
+
+
+		Random random = new Random();
+		for (int i = 0; i < numColumns; i++) {
+			for (int j = 0; j < numRows; j++) {
+				cellChecked[i][j] = random.nextInt(3)-1;
+
+			}
+		}
+
+		invalidate();
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+
+		canvas.drawColor(Color.WHITE);
+
+		if (numColumns == 0 || numRows == 0)
+			return;
+
+		int width = getWidth();
+		int height = getHeight();
+
+		for (int i = 0; i < numColumns; i++) {
+			for (int j = 0; j < numRows; j++) {
+
+
+				int value = cellChecked[i][j];
+				String text = String.valueOf(value);
+				float padding = blackPaint.measureText(text);
+
+				if (value == 0) {
+					blackPaint.setColor(Color.BLACK);
+				}else if (0 < value) {
+					blackPaint.setColor(Color.GREEN);
+				}else  {
+					blackPaint.setColor(Color.RED);
+				}
+
+					canvas.drawText(text, (i + .5f) * cellWidth - padding / 2, (j + .6f) * cellHeight, blackPaint);
+			}
+		}
+		blackPaint.setColor(Color.BLACK);
+		for (int i = 0; i <= numColumns; i++) {
+			canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, blackPaint);
+		}
+
+		for (int i = 0; i <= numRows; i++) {
+			canvas.drawLine(0, i * cellHeight, width, i * cellHeight, blackPaint);
+		}
+		float y = height - 1;
+		canvas.drawLine(0, y, getWidth(), y, blackPaint);
+	}
+}
